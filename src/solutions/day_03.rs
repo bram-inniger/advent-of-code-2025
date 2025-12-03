ef1dbe29ca95a0e02b4cc9151ed46bb2c1cc3206
Bram Inniger <bram@inniger.be>
2025-12-03 16:05:02 +0200

    Solved D03P02

--- src/solutions/day_03.rs
+++ src/solutions/day_03.rs
@@ -1,43 +1,55 @@
 use crate::util::BASE_10;
 use itertools::Itertools;
 
-pub fn solve_1(banks: &[&str]) -> u32 {
+pub fn solve_1(banks: &[&str]) -> u64 {
+    solve(banks, 2)
+}
+
+pub fn solve_2(banks: &[&str]) -> u64 {
+    solve(banks, 12)
+}
+
+fn solve(banks: &[&str], batteries_count: usize) -> u64 {
     banks
         .iter()
         .map(|bank| Bank::new(bank))
-        .map(|bank| bank.max_joltage())
+        .map(|bank| bank.max_joltage(batteries_count))
         .sum()
 }
 
 #[derive(Debug, Clone)]
 struct Bank {
-    batteries: Vec<u32>,
+    batteries: Vec<u64>,
 }
 
 impl Bank {
     pub fn new(batteries: &str) -> Self {
         let batteries = batteries
             .chars()
-            .map(|battery| battery.to_digit(BASE_10).unwrap())
+            .map(|battery| battery.to_digit(BASE_10).unwrap().into())
             .collect_vec();
         Self { batteries }
     }
 
-    pub fn max_joltage(&self) -> u32 {
-        fn largest_battery(batteries: &[u32]) -> (usize, u32) {
-            batteries
+    pub fn max_joltage(&self, batteries_count: usize) -> u64 {
+        let mut joltage = 0;
+        let mut start_idx = 0;
+        let mut end_idx = self.batteries.len() - batteries_count + 1;
+
+        for _ in 0..batteries_count {
+            let (idx, battery) = self.batteries[start_idx..end_idx]
                 .iter()
                 .enumerate()
                 .max_by(|(idx_1, bat_1), (idx_2, bat_2)| bat_1.cmp(bat_2).then(idx_2.cmp(idx_1)))
                 .map(|(idx, &bat)| (idx, bat))
-                .unwrap()
+                .unwrap();
+
+            joltage = joltage * 10 + battery;
+            start_idx += idx + 1;
+            end_idx += 1;
         }
 
-        let (first_idx, first_battery) =
-            largest_battery(&self.batteries[..self.batteries.len() - 1]);
-        let (_, second_battery) = largest_battery(&self.batteries[first_idx + 1..]);
-
-        first_battery * 10 + second_battery
+        joltage
     }
 }
 
@@ -66,5 +78,26 @@ mod tests {
             .collect_vec();
 
         assert_eq!(16_973, solve_1(&input));
+    }
+
+    #[test]
+    fn day_03_part_02_sample() {
+        let sample = vec![
+            "987654321111111",
+            "811111111111119",
+            "234234234234278",
+            "818181911112111",
+        ];
+
+        assert_eq!(3_121_910_778_619, solve_2(&sample));
+    }
+
+    #[test]
+    fn day_03_part_02_solution() {
+        let input = include_str!("../../inputs/day_03.txt")
+            .lines()
+            .collect_vec();
+
+        assert_eq!(168_027_167_146_027, solve_2(&input));
     }
 }
