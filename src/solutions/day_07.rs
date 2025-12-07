f71bdb70d5abe5be609442e4117c425dbc0acbbe
Bram Inniger <bram@inniger.be>
2025-12-07 18:44:22 +0200

    Solved D07P02

--- src/solutions/day_07.rs
+++ src/solutions/day_07.rs
@@ -1,7 +1,11 @@
-use rustc_hash::FxHashSet;
+use rustc_hash::{FxHashMap, FxHashSet};
 
 pub fn solve_1(manifold: &[&str]) -> usize {
     Manifold::new(manifold).beam_splits()
+}
+
+pub fn solve_2(manifold: &[&str]) -> u64 {
+    Manifold::new(manifold).timeline_splits()
 }
 
 #[derive(Debug, Clone)]
@@ -76,6 +80,44 @@ impl Manifold {
 
         splitters.len()
     }
+
+    pub fn timeline_splits(&self) -> u64 {
+        self.timeline_splits_helper(&self.entry, &mut FxHashMap::default())
+    }
+
+    fn timeline_splits_helper(
+        &self,
+        position: &Position,
+        timelines_at: &mut FxHashMap<Position, u64>,
+    ) -> u64 {
+        if position.y == self.depth {
+            return 1;
+        }
+        if timelines_at.contains_key(position) {
+            return timelines_at[position];
+        }
+
+        let timelines = (if self.splitters.contains(position) {
+            vec![
+                Position::of(position.x - 1, position.y),
+                Position::of(position.x + 1, position.y),
+            ]
+        } else {
+            vec![Position::of(position.x, position.y + 1)]
+        })
+        .iter()
+        .map(|p| self.timeline_splits_helper(p, timelines_at))
+        .sum();
+
+        timelines_at.insert(*position, timelines);
+        timelines
+    }
+}
+
+impl Position {
+    fn of(x: usize, y: usize) -> Self {
+        Self { x, y }
+    }
 }
 
 #[cfg(test)]
@@ -115,5 +157,38 @@ mod tests {
             .collect_vec();
 
         assert_eq!(1_651, solve_1(&input));
+    }
+
+    #[test]
+    fn day_07_part_02_sample() {
+        let sample = vec![
+            ".......S.......",
+            "...............",
+            ".......^.......",
+            "...............",
+            "......^.^......",
+            "...............",
+            ".....^.^.^.....",
+            "...............",
+            "....^.^...^....",
+            "...............",
+            "...^.^...^.^...",
+            "...............",
+            "..^...^.....^..",
+            "...............",
+            ".^.^.^.^.^...^.",
+            "...............",
+        ];
+
+        assert_eq!(40, solve_2(&sample));
+    }
+
+    #[test]
+    fn day_07_part_02_solution() {
+        let input = include_str!("../../inputs/day_07.txt")
+            .lines()
+            .collect_vec();
+
+        assert_eq!(108_924_003_331_749, solve_2(&input));
     }
 }
